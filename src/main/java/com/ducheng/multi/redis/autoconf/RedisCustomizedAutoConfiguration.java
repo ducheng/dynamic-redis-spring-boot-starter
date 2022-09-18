af66ef09e6532b49f45f52d65d22df6f5d0cfbbb
USER-20200810GJ\Administrator <13275768320@163.com>
2022-09-18 18:40:10 +0800

    代码完成

--- src/main/java/com/ducheng/multi/redis/autoconf/RedisCustomizedAutoConfiguration.java
+++ src/main/java/com/ducheng/multi/redis/autoconf/RedisCustomizedAutoConfiguration.java
@@ -1,6 +1,7 @@
 package com.ducheng.multi.redis.autoconf;
 
 import com.ducheng.multi.redis.aop.MuLtiRedisInterceptor;
+import com.ducheng.multi.redis.aop.MultiRedisMethodPointcutAdvisor;
 import com.ducheng.multi.redis.factory.MultiRedisConnectionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.AutoConfigureBefore;
@@ -11,16 +12,23 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.core.Ordered;
 
 @Configuration(proxyBeanMethods = false)
 @Import(RedisCustomizedConfiguration.class)
 @AutoConfigureBefore(RedisAutoConfiguration.class)
 @EnableConfigurationProperties(MultiRedisProperties.class)
 public class RedisCustomizedAutoConfiguration {
-    
+
     @Bean
     @ConditionalOnMissingBean
     public MuLtiRedisInterceptor lockInterceptor(MultiRedisConnectionFactory multiRedisConnectionFactory) {
         return new MuLtiRedisInterceptor(multiRedisConnectionFactory);
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public MultiRedisMethodPointcutAdvisor lockAnnotationAdvisor(MuLtiRedisInterceptor muLtiRedisInterceptor) {
+        return new MultiRedisMethodPointcutAdvisor(muLtiRedisInterceptor, Ordered.HIGHEST_PRECEDENCE);
     }
 }
